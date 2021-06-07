72a4f4aa3a6c3742e5e881372ec077c56c47fb82
Pornchai Chaochoungchote <pornchai.chao@truedigital.com>
2021-06-07 22:42:55 +0700

    warpup data

--- routes/beer.js
+++ routes/beer.js
@@ -1,21 +1,32 @@
 var express = require('express')
 var router = express.Router()
 var BeerModel = require('../models/beer')
+var _ = require('lodash')
 
 router.get('/random', async (req, res) => {
   try {
-    let response = []
-    const beer = await BeerModel.findOne({ randomCount: 0 })
-
-    if (beer !== null) {
-      const newUpdate = {
-        $set: {
-          randomCount: 1,
-        },
+    let response = {}
+    const beer = await BeerModel.findOne()
+    if (!_.isEmpty(beer)) {
+      //   // const rawData = await BeerModel.findByIdAndUpdate(beer._id,  })
+      const rawData = beer
+      const { randomCount, _id, uid, brand, name, style, hop, yeast, malts, ibu, alcohol, blg } = rawData
+      await BeerModel.findOneAndUpdate({ _id: _id }, { $inc: { randomCount: 1 } })
+      response = {
+        id: _id,
+        uid,
+        brand,
+        name,
+        style,
+        hop,
+        yeast,
+        malts,
+        ibu,
+        alcohol,
+        blg,
+        randomCount,
       }
-      response = await BeerModel.findByIdAndUpdate(beer._id, newUpdate)
     }
-
     res.status(200).json(response)
   } catch (error) {
     console.log('error', error)
@@ -24,12 +35,15 @@ router.get('/random', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const payload = req.body
-  try {
-    const beer = new BeerModel(payload)
+  console.log('payload', payload)
+  const beer = new BeerModel(payload)
+  const error = beer.validateSync()
+  if (_.isUndefined(error)) {
     await beer.save()
-    res.status(201).end()
-  } catch (error) {
+    res.status(201).json({ message: 'Created!!' })
+  } else {
     console.log('error', error)
+    res.status(400).json({ message: 'Fail to create !!' })
   }
 })
 
