33df59d536f057b2598febdc290e0fb7265e5dc7
Pornchai Chaochoungchote <pornchai.chao@truedigital.com>
2021-06-07 22:42:39 +0700

    change schema

--- models/beer.js
+++ models/beer.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const beerSchema = new Schema({
-  uid: String,
-  brand: String,
-  name: String,
-  style: String,
-  hop: String,
-  yeast: String,
-  malts: String,
-  ibu: String,
-  alcohol: String,
-  blg: String,
-  randomCount: { type: Number, default: 0 },
+  uid: { type: String, required: true, unique: true },
+  brand: { type: String, required: true },
+  name: { type: String, required: true },
+  style: { type: String, required: true },
+  hop: { type: String, required: true },
+  yeast: { type: String, required: true },
+  malts: { type: String, required: true },
+  ibu: { type: String, required: true },
+  alcohol: { type: String, required: true },
+  blg: { type: String, required: true },
+  randomCount: { type: Number, required: false, default: 0 },
 })
 
-const BeerModel = mongoose.model('Beer', beerSchema)
+const BeerModel = mongoose.model('BeerCraft', beerSchema)
 
 module.exports = BeerModel
